5fc76250849f9067b82627a03763b89ff04fd51c
SudhanshuBlaze <skrourkela@gmail.com>
2024-02-23 16:18:04 +0100

    fix: include ip address of workers in /etc/hosts

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,14 @@
 FROM ubuntu:20.04
 
-RUN apt-get update
-RUN apt-get install openjdk-8-jdk -y
-RUN apt-get install -y wget
-RUN apt-get install -y openssh-server
-RUN wget https://downloads.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz -P ~/Downloads
-RUN tar zxvf ~/Downloads/hadoop-3.3.6.tar.gz  -C /usr/local
-RUN mv /usr/local/hadoop-* /usr/local/hadoop
+RUN apt-get update && apt-get install -y \
+    openjdk-8-jdk \
+    wget \
+    openssh-server
 
-RUN mkdir /var/hadoop
-
+RUN wget https://downloads.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6.tar.gz -P ~/Downloads \
+    && tar zxvf ~/Downloads/hadoop-3.3.6.tar.gz  -C /usr/local \
+    && mv /usr/local/hadoop-* /usr/local/hadoop \
+    && mkdir /var/hadoop
 
 ENV  JAVA_HOME /usr/lib/jvm/java-8-openjdk-amd64/jre/
 ENV  PATH $PATH:$JAVA_HOME/bin
@@ -22,13 +21,12 @@ RUN ssh-keygen -t rsa -f ~/.ssh/id_rsa -P '' && \
 
 COPY conf/* /tmp/
 
-RUN mv /tmp/ssh_config ~/.ssh/config
-RUN mv /tmp/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml
-RUN mv /tmp/mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml
-RUN mv /tmp/hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml
-RUN mv /tmp/yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml
-RUN mv /tmp/workers /usr/local/hadoop/etc/hadoop/workers
-
-RUN /tmp/env.sh
+RUN mv /tmp/ssh_config ~/.ssh/config \
+    && mv /tmp/core-site.xml /usr/local/hadoop/etc/hadoop/core-site.xml \
+    && mv /tmp/mapred-site.xml /usr/local/hadoop/etc/hadoop/mapred-site.xml \
+    && mv /tmp/hdfs-site.xml /usr/local/hadoop/etc/hadoop/hdfs-site.xml \
+    && mv /tmp/yarn-site.xml /usr/local/hadoop/etc/hadoop/yarn-site.xml \
+    && mv /tmp/workers /usr/local/hadoop/etc/hadoop/workers \
+    && /tmp/env.sh
 
 CMD [ "sh", "-c", "service ssh start; bash"]
--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#set default worker container count if its unset
+#set default worker container count if it's unset
 if [ -z "$1" ]; then
     workerCount=1
 else
@@ -16,21 +16,39 @@ for (( i=1; i<=$workerCount; i++ )); do
     echo "worker$i" >> ./conf/workers
 done
 
-# Create a network named "hadoopNetwork"
-docker network rm hadoopNetwork && docker network create -d bridge --subnet 172.25.0.0/16 hadoopNetwork
-
 # Create base hadoop image named "base-hadoop:1.0"
 docker build -t base-hadoop:1.0 .
 
 # Run base-hadoop:1.0 image as master container
-docker run -itd --network="hadoopNetwork" --ip 172.25.0.100 -p 9870:9870 -p 8088:8088 -p 9864:9864 --name master --hostname master base-hadoop:1.0
+master_container_id=$(docker run -itd -p 9870:9870 -p 8088:8088 -p 9864:9864 --name master --hostname master base-hadoop:1.0)
+
+master_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $master_container_id)
 
 for (( c=1; c<=$workerCount; c++ )); do
     tmpName="worker$c"
     # Run base-hadoop:1.0 image as worker container
-    docker run -itd --network="hadoopNetwork" --ip "172.25.0.10$c" --name $tmpName --hostname $tmpName base-hadoop:1.0
+    worker_container_id=$(docker run -itd --name $tmpName --hostname $tmpName base-hadoop:1.0)
+    worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $worker_container_id)
+    # Update /etc/hosts file of worker container with entries for master and workers
+    docker exec -it $worker_container_id bash -c "echo '$master_ip master' >> /etc/hosts"
+done
+
+# Update /etc/hosts file of master container with entries for workers
+for (( c=1; c<=$workerCount; c++ )); do
+    tmpName="worker$c"
+    worker_ip=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $tmpName)
+    # Add worker IPs and hostnames to /etc/hosts of master container
+    docker exec -it $master_container_id bash -c "echo '$worker_ip $tmpName' >> /etc/hosts"
+
+    # add worker hostnames and IPs to /etc/hosts of each worker container to each other worker container
+    for (( d=1; d<=$workerCount; d++ )); do
+        tmpName2="worker$d"
+        worker_ip2=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' $tmpName2)
+        docker exec -it $tmpName bash -c "echo '$worker_ip2 $tmpName2' >> /etc/hosts"
+    done
+
 done
 
 # Run hadoop commands
-docker exec -ti master bash -c "hadoop namenode -format && /usr/local/hadoop/sbin/start-dfs.sh && yarn --daemon start resourcemanager && yarn --daemon start nodemanager && mapred --daemon start historyserver"
-docker exec -ti master bash
+docker exec -ti $master_container_id bash -c "hadoop namenode -format && /usr/local/hadoop/sbin/start-dfs.sh && yarn --daemon start resourcemanager && yarn --daemon start nodemanager && mapred --daemon start historyserver"
+docker exec -ti $master_container_id bash
